61a039e90ded1f44683713f5e8fba97c3cf7c906
gabryel <valvanogabryel@gmail.com>
2023-06-14 14:20:43 -0300

    ;

--- tokners/src/App.tsx
+++ tokners/src/App.tsx
@@ -55,26 +55,30 @@ export default function App() {
       <Header scrolled={scrolled} />
       <main>
         <BlockchainSection />
-        <Container>
-          <TwoColumns>
-            <CardFor role="Criadores">
-              Os criadores podem obter independência por meio de um sistema de
-              moeda digital descentralizado que é dependente de crescer e se
-              envolver com a comunidade e também com seu poder de estrela. Eles
-              possuem 10-15% do valor total dos tokens cunhados.
-            </CardFor>
-            <CardFor role="Hodlers">
-              Manter tokens sociais permite que o indivíduo tenha acesso a
-              benefícios, incluindo conteúdo, comunidades privadas, acesso
-              direto a celebridades, acesso antecipado a ingressos e muito mais
-              como a capacidade de negociar com outras comunidades para obter
-              acesso a mais conteúdo do criador com primeiros compradores de
-              token sendo os maiores vencedores, pois o valor do token aumenta
-              com mais compradores.
-            </CardFor>
-            <BlackWaves />
-          </TwoColumns>
-        </Container>
+
+        <OuterContainer>
+          <BlackWaves />
+
+          <Container>
+            <TwoColumns>
+              <CardFor role="Criadores">
+                Os criadores podem obter independência por meio de um sistema de
+                moeda digital descentralizado que é dependente de crescer e se
+                envolver com a comunidade e também com seu poder de estrela.
+                Eles possuem 10-15% do valor total dos tokens cunhados.
+              </CardFor>
+              <CardFor role="Hodlers">
+                Manter tokens sociais permite que o indivíduo tenha acesso a
+                benefícios, incluindo conteúdo, comunidades privadas, acesso
+                direto a celebridades, acesso antecipado a ingressos e muito
+                mais como a capacidade de negociar com outras comunidades para
+                obter acesso a mais conteúdo do criador com primeiros
+                compradores de token sendo os maiores vencedores, pois o valor
+                do token aumenta com mais compradores.
+              </CardFor>
+            </TwoColumns>
+          </Container>
+        </OuterContainer>
 
         <Container>
           <WhyUs />
--- tokners/src/components/BlackWaves.tsx
+++ tokners/src/components/BlackWaves.tsx
@@ -2,16 +2,15 @@ import blackWaves from "../assets/cardsFor/waves_background.png";
 import styled from "styled-components";
 
 export const BlackWaves = styled.div`
-  background-position: center center;
+  background-position: left center;
   background-repeat: no-repeat;
   background-size: cover;
   background-image: url(${blackWaves});
-  height: 120%;
-  width: 117.8%;
+  height: 1212px;
+  width: 100%;
   position: absolute;
   top: 50%;
   right: 0;
-  left: -9%;
   bottom: 0;
   z-index: -9999;
 `;
